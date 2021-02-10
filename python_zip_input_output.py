fe60768c736c110573e1c1aa6fd614b5115c4282
Hidde <77933473+githubhidde@users.noreply.github.com>
2021-02-10 20:31:50 +0100

    Update python_zip_input_output.py

--- python_zip_input_output.py
+++ python_zip_input_output.py
@@ -1,9 +1,14 @@
 from zipfile import ZipFile
 import os
+if not os.path.exists(r'd:\_test_dir'):
+	os.makedirs(r'd:\_test_dir')
+	print(r"D:\test_dir" "is created!")
+else:
+	print(r'D:\_test_dir' + ' already existed')
 
 # create a ZipFile object
 zipObj = ZipFile(r'd:\_test_dir\sample.zip', 'w')
-print("d:\_test_dir\sample.zip' is temporarily created")
+print("D:\_test_dir\sample.zip' is temporarily created")
 # Add multiple files to the zip
 zipObj.write(r'C:\_test_dir\test123.txt')
 zipObj.write(r'C:\_test_dir\test_1.log')
@@ -13,18 +18,20 @@ print("log files are added to the zip file")
 zipObj.close()
 
 # Extract the zip file to a given location
-zf = ZipFile(r'd:\_test_dir\sample.zip', 'r')
-zf.extractall(r'c:\EDV')
-print("Content is written in " + r'c:\EDV\test_dir')
+zf = ZipFile(r'D:\_test_dir\sample.zip', 'r')
+zf.extractall(r'C:\EDV')
+print("Content is written in " + r'C:\EDV\test_dir')
 zf.close()
 print('Done!') 
 
 # Delete temporary file location
-myfile=r'd:\_test_dir\sample.zip'
+myfile=r'D:\_test_dir\sample.zip'
 
 ## If file exists, delete it ##
 if os.path.isfile(myfile):
 	os.remove(myfile)
-	print("r'd:\_test_dir\sample.zip' + is cleaned up!")
+	print(r'D:\_test_dir\sample.zip' + ' is cleaned up!')
+	os.rmdir(f'D:\_test_dir')
+	print(f'D:\_test_dir' + ' is removed!')
 else:    ## Show an error ##
 	print("Error: %s file not found" % myfile)
