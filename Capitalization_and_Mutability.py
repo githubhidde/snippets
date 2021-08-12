0f9e930ea77af9446619a9796e3b5ec7b1862277
Hidde <77933473+githubhidde@users.noreply.github.com>
2021-08-12 18:21:29 +0200

    Add files via upload

---
+++ Capitalization_and_Mutability.py
@@ -0,0 +1,9 @@
+# codewars challenge, check out the URL:
+# https://www.codewars.com/kata/595970246c9b8fa0a8000086/train/python
+    # A very beginner friendly challenge. Where you can practice slicing.
+def capitalize_word (word):
+    capital = word[:1].upper()
+    return capital + word[1:]
+
+capitalize_word('i')
+        
