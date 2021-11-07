1b0014a23a0ce8ae4e2b68e0b431721d93e18e0c
Hidde <77933473+githubhidde@users.noreply.github.com>
2021-11-07 15:54:35 +0100

    Add files via upload

---
+++ Bash Basics - While Loop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# codewars challenge, check out the URL:
+# https://www.codewars.com/kata/582cd9033c1acf1d45000052
+
+countToTwenty() {
+for i in {1..20}; do
+    echo Count: $i
+done
+}
+
+countToTwenty
